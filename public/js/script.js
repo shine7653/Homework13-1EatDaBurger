05105d87f7758277ab2319964997053259bd08c9
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2020-01-20 11:04:29 -0800

    working

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   
       var burger_id = $(this).children(".burger_id").val();
       console.log(burger_id);
+      
       $.ajax({
         method: "PUT",
         url: "/burgers/" + burger_id
